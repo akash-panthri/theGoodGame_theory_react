745909977b9e258dce43ef6c87e8cea837fedaeb
akash-panthri <akashpanthri2000@gmail.com>
2024-07-05 13:12:38 +0530

    Made Basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [showSide, setShowSide] = useState(false);
+  const [error, setError] = useState(null);
+  const [qoutes, setQoutes] = useState([]);
+
+  const addItem = (newItem) => {
+    setQoutes([...qoutes, newItem]);
+  };
+
+  const fetchQuote=()=>{
+    fetch('https://ron-swanson-quotes.herokuapp.com/v2/quotes')
+    .then(response => response.json())
+    .then(data => {
+      setData(data);
+      setLoading(false);
+    })
+    .catch(err => {
+      setError(err);
+      setLoading(false);
+    });
+  }
+ 
+  useEffect(() => {
+    fetchQuote()
+  
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button  className={loading ? 'loading':'btn'} onClick={()=>setShowSide((prev)=> !prev)} >{showSide ?'Hide' : 'Show' } saved</button>
+
+      <div className="container">
+        <h3>Don't Laugh Challenge</h3>
+        <div id="quote" className="quote">
+{data}
+        </div>
+        <button id="quoteBtn" className={loading ? 'loading':'btn'} onClick={()=>fetchQuote()}>Getquote</button>
+        <button  className={loading ? 'loading':'btn'} onClick={()=>addItem(data)} >Save to list</button>
+        
+    </div>
     </div>
   );
 }
